168898387cccfec473ad11ca2a93aba208bce7eb
Yarles <yarles100@gmail.com>
2025-08-20 13:40:09 -0300

    fix: correção da api da catraca

--- hub/src/Toletus.Hub.API/Program.cs
+++ hub/src/Toletus.Hub.API/Program.cs
@@ -7,22 +7,35 @@ using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-ConfigureServices(builder);
+// Lê config
+var hubCfg = builder.Configuration.GetSection("Hub");
+var bindIpForDevices = hubCfg.GetValue<string>("BindIp"); // usar só para devices
+var port = hubCfg.GetValue<int?>("Port") ?? 5110;
+
+// Kestrel: escuta em todas as interfaces
+builder.WebHost.ConfigureKestrel(o =>
+{
+    o.Listen(IPAddress.Any, port);        // http://0.0.0.0:5110
+    // Se precisar HTTPS, adicionar outro Listen com certificado
+});
+
+ConfigureServices(builder, bindIpForDevices);
 
 var app = builder.Build();
-
 ConfigureMiddlewares(app);
 
 app.Run();
 
-void ConfigureServices(WebApplicationBuilder builder)
+void ConfigureServices(WebApplicationBuilder builder, string? bindIpForDevices)
 {
     InitializeNotificationServices();
 
-    var boards = LiteNet2Devices.SearchLiteNet2Boards( // codigo adicionado manualmente
-    IPAddress.Parse("192.168.4.38") // ip da maquina que se comunica
-    );
-    LiteNet2Devices.SetBoards([.. boards]);
+    // Usa o IP da interface para varrer os devices (NÃO é o IP de bind do servidor)
+    if (!string.IsNullOrWhiteSpace(bindIpForDevices))
+    {
+        var boards = LiteNet2Devices.SearchLiteNet2Boards(IPAddress.Parse(bindIpForDevices));
+        LiteNet2Devices.SetBoards([.. boards]);
+    }
 
     builder.RegisterIoC();
     builder.Services.AddControllers();
@@ -38,10 +51,7 @@ void ConfigureMiddlewares(WebApplication app)
     app.MapControllers();
 }
 
-void ApplicationStopping()
-{
-    Console.WriteLine("The application is shutting down...");
-}
+void ApplicationStopping() => Console.WriteLine("The application is shutting down...");
 
 void InitializeNotificationServices()
 {
