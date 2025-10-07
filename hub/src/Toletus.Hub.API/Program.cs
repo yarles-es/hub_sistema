1a3c6cba9d9cfe47b296119195733bf28b60d028
Yarles <yarles100@gmail.com>
2025-10-07 16:42:12 -0300

    fix: removido comentarios informativos

--- hub/src/Toletus.Hub.API/Program.cs
+++ hub/src/Toletus.Hub.API/Program.cs
@@ -9,14 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Lê config
 var hubCfg = builder.Configuration.GetSection("Hub");
-var bindIpForDevices = hubCfg.GetValue<string>("BindIp"); // usar só para devices
+var bindIpForDevices = hubCfg.GetValue<string>("BindIp");
 var port = hubCfg.GetValue<int?>("Port") ?? 5110;
 
-// Kestrel: escuta em todas as interfaces
 builder.WebHost.ConfigureKestrel(o =>
 {
-    o.Listen(IPAddress.Any, port);        // http://0.0.0.0:5110
-    // Se precisar HTTPS, adicionar outro Listen com certificado
+    o.Listen(IPAddress.Any, port);
 });
 
 ConfigureServices(builder, bindIpForDevices);
@@ -30,7 +28,6 @@ void ConfigureServices(WebApplicationBuilder builder, string? bindIpForDevices)
 {
     InitializeNotificationServices();
 
-    // Usa o IP da interface para varrer os devices (NÃO é o IP de bind do servidor)
     if (!string.IsNullOrWhiteSpace(bindIpForDevices))
     {
         var boards = LiteNet2Devices.SearchLiteNet2Boards(IPAddress.Parse(bindIpForDevices));
